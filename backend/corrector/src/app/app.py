06b8caafa03bb1f3459709ca76f1dee3ed8b8950
JT4V4res <joaotavarestavares@hotmail.com>
2025-03-30 13:32:09 -0300

    Creating the user interface and changing the model loading

--- backend/corrector/src/app/app.py
+++ backend/corrector/src/app/app.py
@@ -1,11 +1,35 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from pred.essay_correction import *
+# import tensorflow as tf
+import nltk
+from app.model_loader import load_model
+import spacy
+from transformers import AutoTokenizer
 
 app = FastAPI(title="Essay correction API")
 
+model = None
+nlp = None
+
+tokenizer = None
+
 class Essay(BaseModel):
     text: str
+
+@app.on_event("startup")
+async def startup_libraries():
+    global model, nlp, tokenizer
+    model = load_model()
+
+    tokenizer = AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased')
+
+    nlp = spacy.load("pt_core_news_lg")
+
+    try:
+        nltk.download('stopwords')
+    except:
+        print('stopwords already downloaded')
 
 @app.post("/predict/tf/", status_code=200)
 async def predict_essay(request: Essay):
